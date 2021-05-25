801fd838fca06ce432f9ce7527fd2758b77b5841
Thorsten Blum <thorsten.blum@toblux.com>
2021-05-25 10:23:29 +0200

    Remove explicit hostname, port, and socket

--- clamav-client-test/clamav-client.rkt
+++ clamav-client-test/clamav-client.rkt
@@ -1,35 +1,40 @@
 #lang racket/base
 
-;; Clamd must be running for the tests to pass
-(define test-hostname "localhost")
-(define test-port 3310)
-(define test-socket-path "/tmp/clamd.socket")
+;;; clamd must be running and accepting
+;;; TCP connections at "localhost:3310" and
+;;; Unix socket connections at "/tmp/clamd.socket"
+;;; for the tests to pass.
 
 (require rackunit
          racket/unix-socket
          clamav-client)
 
+(define eicar-test-filename "eicar.txt")
+(define clean-test-filename "clamav-client.rkt")
+
+(define stream-ok-response #"stream: OK\0")
+(define eicar-found-response #"stream: Eicar-Signature FOUND\0")
+
+(check-false (clean? eicar-found-response))
+(check-true (clean? stream-ok-response))
+
 (test-case
   "Responds to ping with pong"
   (let ([pong-response #"PONG\0"])
-    (check-equal? (ping-tcp test-hostname test-port) pong-response)
+    (check-equal? (ping-tcp) pong-response)
     (when unix-socket-available?
-      (check-equal? (ping-socket test-socket-path) pong-response))))
-
-(define eicar-test-filename "eicar.txt")
+      (check-equal? (ping-socket) pong-response))))
 
 (test-case
   "Eicar signature is found"
-  (let ([scan-result #"stream: Eicar-Signature FOUND\0"])
-    (check-equal? (scan-file-tcp eicar-test-filename test-hostname test-port) scan-result)
-    (when unix-socket-available?
-      (check-equal? (scan-file-socket eicar-test-filename test-socket-path) scan-result))))
+  (check-equal? (scan-file-tcp eicar-test-filename) eicar-found-response)
+  (when unix-socket-available?
+    (check-equal? (scan-file-socket eicar-test-filename) eicar-found-response)))
 
 (test-case
   "clean? works"
-  (let ([src-filename "clamav-client.rkt"])
-    (check-false (clean? (scan-file-tcp eicar-test-filename test-hostname test-port)))
-    (check-true (clean? (scan-file-tcp src-filename test-hostname test-port)))
-    (when unix-socket-available?
-      (check-false (clean? (scan-file-socket eicar-test-filename test-socket-path)))
-      (check-true (clean? (scan-file-socket src-filename test-socket-path))))))
+  (check-false (clean? (scan-file-tcp eicar-test-filename)))
+  (check-true (clean? (scan-file-tcp clean-test-filename)))
+  (when unix-socket-available?
+    (check-false (clean? (scan-file-socket eicar-test-filename)))
+    (check-true (clean? (scan-file-socket clean-test-filename)))))
